d6f36cb241012f9f2ff2ccc50e222a4b569b1871
Tyler Martinez <slightlytyler@gmail.com>
2016-01-08 16:10:52 -0600

    A better way of fetching records

--- src/pods/sith/actions.js
+++ src/pods/sith/actions.js
@@ -1,32 +1,42 @@
 import request from 'superagent';
 
 import { actionTypes } from './constants';
-const { FETCH_SITH } = actionTypes;
+const { FETCH_SITH, SET_SITH } = actionTypes;
 
 export function fetchSith (id) {
-  const url = `http://localhost:3000/dark-jedis/${id}`;
-
   return (dispatch) => {
     dispatch({
       type: FETCH_SITH,
       id
     });
 
-    return request
-      .get(url)
-      .end((err, res) => {
-        if (err) {
-          return dispatch({
-            type: 'SET_SITH',
-            error: err
-          });
-        } else {
-          return dispatch({
-            type: 'SET_SITH',
-            entity: res.body
-          });
-        }
-      })
-    ;
+    return findRecord(id).then((response, err) => {
+      if (err) {
+        console.log(`Could not fetch the sith with id ${id}`);
+        console.log(err);
+
+        return dispatch({
+          type: SET_SITH,
+          error: err
+        });
+      } else {
+        return dispatch({
+          type: SET_SITH,
+          entity: response.body
+        });
+      }
+    });
   };
+}
+
+function findRecord (id) {
+  const url = `http://localhost:3000/dark-jedis/${id}`;
+
+  return new Promise((resolve, reject) => {
+    request
+      .get(url)
+      .end((err, response) =>
+        err ? reject(err) : resolve(response)
+      );
+  });
 }
