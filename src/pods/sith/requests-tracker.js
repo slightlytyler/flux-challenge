7842fc6fe66e3043675cde6995885f85650f48ad
Tyler Martinez <slightlytyler@gmail.com>
2016-01-13 14:14:50 -0600

    Properly cancel requests

--- src/pods/sith/requests-tracker.js
+++ src/pods/sith/requests-tracker.js
@@ -1,4 +1,5 @@
 import request from 'superagent';
+import { difference } from 'lodash';
 
 import {
   updateSith,
@@ -13,6 +14,17 @@ export default function (store) {
     const { sith } = store.getState();
     const unloadedSith = sith.filter(entity => entity.id && !(entity.loading || entity.loaded));
 
+    // First need to cancel all requests for sith not in the current state
+    const currentSithRefs = sith.map(entity => entity.ref)
+    const currentLoadingSithRefs = Object.keys(requests);
+    const unneededRequestRefs = difference(currentLoadingSithRefs, currentSithRefs);
+    console.log(currentLoadingSithRefs);
+    console.log(currentSithRefs);
+    unneededRequestRefs.forEach(ref => {
+      requests[ref].abort();
+      delete requests[ref];
+    });
+
     unloadedSith.forEach(entity => {
       dispatch(updateSith(entity.ref, {
         loading: true,
@@ -20,43 +32,50 @@ export default function (store) {
         error: false
       }));
 
-      findRecord(entity.id).then((response, error) => {
-        if (response) {
-          dispatch(
-            updateSith(
-              entity.ref,
-              Object.assign({}, response, {
-                loading: false,
-                loaded: true,
-                error: false
-              })
-            )
-          );
+      let url = `http://localhost:3000/dark-jedis/${entity.id}`;
 
-          const { master, apprentice } = response;
+      requests[entity.ref] = request
+        .get(url)
+        .end((error, response) => {
+          if (response) {
+            delete requests[entity.id];
 
-          if (master && master.id && !sith.some(entity => entity.id === master.id)) {
-            dispatch(replaceSith(master.id, 'master'));
+            dispatch(
+              updateSith(
+                entity.ref,
+                Object.assign({}, response.body, {
+                  loading: false,
+                  loaded: true,
+                  error: false
+                })
+              )
+            );
+
+            const { master, apprentice } = response.body;
+
+            if (master && master.id && !sith.some(entity => entity.id === master.id)) {
+              dispatch(replaceSith(master.id, 'master'));
+            }
+
+            if (apprentice && apprentice.id && !sith.some(entity => entity.id === apprentice.id)) {
+              dispatch(replaceSith(apprentice.id, 'apprentice'));
+            }
+          } else if (error) {
+            console.log(error);
+
+            dispatch(
+              updateSith(
+                entity.ref,
+                {
+                  loading: false,
+                  loaded: false,
+                  error
+                }
+              )
+            );
           }
-
-          if (apprentice && apprentice.id && !sith.some(entity => entity.id === apprentice.id)) {
-            dispatch(replaceSith(apprentice.id, 'apprentice'));
-          }
-        } else if (error) {
-          console.log(error);
-
-          dispatch(
-            updateSith(
-              entity.ref,
-              {
-                loading: false,
-                loaded: false,
-                error
-              }
-            )
-          );
-        }
-      });
+        })
+      ;
     });
   };
 }
